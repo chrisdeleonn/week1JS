0fcde8167382615170bb5a3b6f483ebdb948f30b
Chris De Leon <72676233+chrisdeleonn@users.noreply.github.com>
2021-07-12 08:56:59 -0400

    Create practice.js
    day02

---
+++ practice.js
@@ -0,0 +1,58 @@
+//func that adds 2 numbers- how i did it
+
+let carHeight = 1
+let truckHeight = 9
+
+function calcHeightTotals(height1, height2) {
+    return height1 + height2
+}
+
+(calcHeightTotals(carHeight, truckHeight))
+
+
+//how michelle did it
+
+const num1 = 2
+const num2 = 3
+
+function addNums(a, b) {
+    return a + b
+}
+addNums(num1, num2)
+
+//func to return the greater of 2 numbers - how i did it
+
+let delrayBeach = 50;
+if (delrayBeach < 40){
+    return ("this is a bad beach")
+} else { 
+    ("this is a good beach")
+}
+
+//trying to copy code somewhere else
+function isGreater(beach) {
+    if(delrayBeach > 40) {
+        console.log("*** this is a bad beach. ***")
+    } else {
+        console.log("this is a good beach.")
+    }
+}
+
+isGreater(beach)
+
+
+//how michelle did it
+function findGreatest(a, b) {
+    if(a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+const num1 = 2
+const num2 = 4
+
+console.log(findGreatest(num1, num2))
+
+
