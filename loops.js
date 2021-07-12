cb1ef9d9981f44375af2c3252c173103a9bfa19b
Chris De Leon <72676233+chrisdeleonn@users.noreply.github.com>
2021-07-12 08:55:54 -0400

    Create loops.js
    day02

---
+++ loops.js
@@ -0,0 +1,25 @@
+//example - for
+for (let i = 0; i < 10; i++) {
+    console.log("this is how we do it", i)
+}
+
+//example #2 - for
+const nums = [10, 20, 30, 40, 50]
+
+for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i])
+}
+
+//example #3 - do while
+let i = 0
+do {
+    i++
+    console.log("this is how we do it", i)
+} while (i < 10)
+
+//example #4 - while
+let i = 0
+while (i < 10) {
+    i++
+    console.log("this is how we do it", i)
+}
