e1ea30663bd30b6fb760514a510db87790499d77
Chris De Leon <72676233+chrisdeleonn@users.noreply.github.com>
2021-07-12 08:54:01 -0400

    Create array.js
    day02

---
+++ array.js
@@ -0,0 +1,25 @@
+//arrays
+let fruits = ["apple", "banana", "orange"]
+let list = ["Boca Code", 24, true, [11]]
+
+console.log(fruits[0]) //apples
+console.log(fruits[1]) //bananas
+console.log(fruits[2]) //oranges
+
+//returns the # of items in the array
+.length
+
+//object array reference
+let staff = [{
+    firstName: "Michelle",
+    subject: "React",
+    awesome: true
+}, {
+    firstName: "Jonathan",
+    subject: "Mobile",
+    awesome: true
+}]
+
+console.log(staff[1].subject) //mobile
+
+//
