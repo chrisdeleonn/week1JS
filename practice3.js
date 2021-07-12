60cc924b28a970a3150d4db68d45d5b00440a315
Chris De Leon <72676233+chrisdeleonn@users.noreply.github.com>
2021-07-12 08:58:38 -0400

    Create practice3.js
    day02

---
+++ practice3.js
@@ -0,0 +1,7 @@
+function squaredNums(myArray) {
+    for (let i = 0; i < myArray.length; i++) {
+        console.log(myArray[i] ** 2)
+    } 
+}
+    const listOfNum = [2, 5, 7, 9, 11]
+    squaredNums(listOfNum)
